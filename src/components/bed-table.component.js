5de98eb08cc74315b9d3d14ead604684df74b006
KDShah <kundanshah71@gmail.com>
2021-05-16 20:14:48 +0545

    refactor: add verified field

--- src/components/bed-table.component.js
+++ src/components/bed-table.component.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Moment from "react-moment";
 
 const BedsTable = ({ coas }) => (
   <div className="table-responsive ">
     <table className="table table-hover table-borderless">
       <thead>
         <tr>
-          <th scope="col">#</th>
+          <th scope="col">Beds</th>
           <th className="text-center" scope="col">
             Capacity
           </th>
@@ -15,13 +16,16 @@ const BedsTable = ({ coas }) => (
           <th className="text-center" scope="col">
             Available
           </th>
+          <th className="text-center" scope="col">
+            Verified
+          </th>
         </tr>
       </thead>
       <tbody>
         {coas.map((coa, index) => {
           const {
             name,
-            data: { capacity, occupied, available },
+            data: { capacity, occupied, available, updated_at },
           } = coa;
           return (
             <tr key={index}>
@@ -29,6 +33,9 @@ const BedsTable = ({ coas }) => (
               <td className="text-center">{capacity}</td>
               <td className="text-center">{occupied}</td>
               <td className="text-center">{available}</td>
+              <td className="text-center text-muted">
+                <Moment fromNow>{updated_at}</Moment>
+              </td>
             </tr>
           );
         })}
