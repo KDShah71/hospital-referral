dad212a8eb37c6ff27bf0d6577a7b53c954d035d
KDShah <kundanshah71@gmail.com>
2021-05-16 19:18:55 +0545

    refactor: date

--- src/components/check-icu.component.js
+++ src/components/check-icu.component.js
@@ -4,7 +4,7 @@ const CheckIcu = ({ icu, state }) => {
   return (
     <div className="d-flex flex-column align-items-start">
       <div>
-        <span className="p-14 text-black-50 me-2">ICU </span>
+        <span className="p-12 text-black-50 me-2">ICU </span>
         <span
           className={`px-3 mx-2 py-1 badge  ${
             state ? "bg-success" : "bg-danger"
@@ -15,8 +15,8 @@ const CheckIcu = ({ icu, state }) => {
       </div>
       {icu && (
         <p className="text-black-50 m-0 pt-2 d-flex justify-content-start p-12">
-          updated time : {icu.updated_at.slice(11, 16)} /{" "}
-          {icu.updated_at.slice(0, 10)}
+          Verified* : {icu.updated_at.slice(11, 16)} /{" "}
+          {icu.updated_at.slice(5, 10)}
         </p>
       )}
     </div>
