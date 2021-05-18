e8b7abb1a02f2516dc203219f7299b77f6ba2963
KDShah <kundanshah71@gmail.com>
2021-05-18 21:06:50 +0545

    feat: scale card on hover

--- src/components/card.component.js
+++ src/components/card.component.js
@@ -22,7 +22,7 @@ function DetailsCard({ title = "Title", to, data = {} }) {
         return;
       }}
     >
-      <div className="col py-3 px-4  card shadow-sm  border bg-light ">
+      <div className="col scale py-3 px-4  card shadow-sm  border bg-light ">
         <h5 className="card-title"> {title} </h5>
 
         <div className="card-text ">
