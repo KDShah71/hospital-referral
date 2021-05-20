9fec720ef87a8e259684e489d5e84ec055f2a104
KDShah <kundanshah71@gmail.com>
2021-05-20 11:29:48 +0545

    typo

--- src/features/home/home.screen.js
+++ src/features/home/home.screen.js
@@ -9,7 +9,7 @@ function Home({ children }) {
     <div className="">
       <div>
         <div className="pt-2 ">
-          <div className="container d-flex justify-content-start  ">
+          <div className="container d-flex justify-content-start ">
             <div className="">
               <a href="/" className="px-3">
                 <img src={logo} alt="hospital referral logo" width="110" />
@@ -21,7 +21,7 @@ function Home({ children }) {
                 <span> By Lumbini Hospitals</span>
               </div>
 
-              <div className="d-flex flex-column align-items-center px-2">
+              <div className="d-flex flex-column  align-items-center px-2">
                 <a
                   href="https://play.google.com/store/apps/details?id=com.hcoe.lumbini_hospital"
                   // target="_blank"
@@ -29,7 +29,7 @@ function Home({ children }) {
                 >
                   <img src={QR} alt="hospital referral logo" width="110" />
                 </a>
-                <p className="p-12 text-black-50 ">scan to download </p>
+                <p className="p-12 m-0 text-black-50 ">scan to download app</p>
               </div>
             </div>
           </div>
